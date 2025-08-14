f7f8a16efe63ab22e6721325424a5e25a4710f8a
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-08-14 23:43:45 +0530

    added a new container component

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
+  return (<>
+  <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <div className="container"></div>
+  </>
+    
   )
 }
