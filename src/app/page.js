27c0fabf1343e209fa3fc8ac2618307ef6af2d3b
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-08-16 22:59:38 +0530

    added a few changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,7 @@ export default function Home() {
   <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <div className="container">
-      <p>code</p>
-    </div>
+    
   </>
     
   )
