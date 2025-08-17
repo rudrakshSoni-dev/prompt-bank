db433bed863cc586eb60b342991a2727ff58678b
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-08-17 23:01:33 +0530

    added something

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (<>
   <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello
     </h1>
     
   </>
